677e8007ccf145c47ab5be1fc5767b0504b667ac
Shawn Tapps <stapps.prsvr@gmail.com>
2024-11-17 18:23:40 -0600

    Empty Structure

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <h1>Space Place</h1>
+        <h2>Test Placeholder</h2>
+      </div>
+    </>
+  );
+}
+
+export default App;
