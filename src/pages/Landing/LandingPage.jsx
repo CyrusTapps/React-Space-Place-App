cbd991ea66d50b6c78d9bdb412df4b094e6098aa
Shawn Tapps <stapps.prsvr@gmail.com>
2024-11-18 20:32:34 -0600

    content, styling, dynamic navcards

--- src/pages/Landing/LandingPage.jsx
+++ src/pages/Landing/LandingPage.jsx
@@ -5,6 +5,41 @@ import NavBar from "../../components/common/Navigation/NavBar";
 import Hero from "../../components/common/Hero/Hero";
 import Footer from "../../components/common/Footer/Footer";
 import NavCard from "../../components/common/Card/NavCard";
+import image1 from "/assets/images/launch.jpg";
+import image2 from "/assets/images/nebula.jpg";
+import image3 from "/assets/images/solsys.jpg";
+import image4 from "/assets/images/iss.jpg";
+
+const cardData = [
+  {
+    image: image1,
+    title: "Space News",
+    description: "Explore the latest news in space",
+    link: "/launch",
+    id: 1,
+  },
+  {
+    image: image2,
+    title: "NASA Imagery",
+    description: "See the beautiful images captured by NASA",
+    link: "/exploration",
+    id: 2,
+  },
+  {
+    image: image3,
+    title: "3D Interactive Solar System",
+    description: "Explore our neighborhood in space",
+    link: "/solar-system",
+    id: 3,
+  },
+  {
+    image: image4,
+    title: "International Space Station",
+    description: "See the ISS current location and live video feed",
+    link: "/iss",
+    id: 4,
+  },
+];
 
 const LandingPage = () => {
   return (
@@ -13,10 +48,15 @@ const LandingPage = () => {
       <NavBar />
       <Hero />
       <main className="main-content">
-        <NavCard />
-        <NavCard />
-        <NavCard />
-        <NavCard />
+        {cardData.map((card) => (
+          <NavCard
+            key={card.id}
+            image={card.image}
+            title={card.title}
+            description={card.description}
+            link={card.link}
+          />
+        ))}
       </main>
 
       <Footer />
